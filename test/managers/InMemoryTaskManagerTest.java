abe17e95571f32a44ddf12c108e32080e271f10a
Artem <artem33volkov@gmail.com>
2024-02-05 12:09:46 +0300

    style: убрать неиспользуемые импорты

--- test/managers/InMemoryTaskManagerTest.java
+++ test/managers/InMemoryTaskManagerTest.java
@@ -1,16 +1,6 @@
 package managers;
 
-import managers.InMemoryTaskManager;
-import managers.Managers;
-import managers.TaskManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tasks.Epic;
-import tasks.Subtask;
-import tasks.Task;
-import tasks.TaskType;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
 
